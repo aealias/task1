6327a95b5619a57169f992fa9fed197672640105
aleeta mandi <aealias@netrixllc.com>
2018-06-25 17:28:58 +0530

    save first commit

---
+++ task1/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace task1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult postAbout()
+        {
+            for (int i = 0; i < Request.Files.Count; i++)
+            {
+                var file = Request.Files[i];
+
+                var fileName = Path.GetFileName(file.FileName);
+                try
+                {
+                    var path = Path.Combine(Server.MapPath("~/images/"), fileName);
+                    file.SaveAs(path);
+                }
+                catch (Exception e)
+                {
+
+                }
+                
+            }
+            return  JsonRequestBehavior
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
